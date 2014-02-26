9778db11a3fcb1ee9e71c955eb2ab279b610c591
Harrison Powers <fijimunkii@gmail.com>
2014-02-25 23:44:10 -0500

    passes a test, it is alive!!

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have no chores to start', function () {
+    expect(scope.chores.length).toBe(0);
   });
 });
